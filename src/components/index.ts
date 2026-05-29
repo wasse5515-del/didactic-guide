a2212d5959a19898c2ff411f433e3a548d7a71da
wasse5515-del <wasse5515@gmail.com>
2026-05-29 04:49:56 -0700

    feat: Add components barrel export

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { AuthForm } from './AuthForm';
+export { CourseDiscovery } from './CourseDiscovery';
+export { LessonViewer } from './LessonViewer';
+export { QuizComponent } from './QuizComponent';
+export { LearnerDashboard } from './LearnerDashboard';
+export { SubscriptionModal } from './SubscriptionModal';
+export { VideoPlayer } from './VideoPlayer';
+export { ProgressBar, CourseProgress } from './ProgressBar';
